b46ff8402bf1c47dc9e1b1f8083c961e046127ea
KB <161012310+karim121100@users.noreply.github.com>
2026-01-05 23:30:40 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function toUpper() {
+  const text = document.getElementById("text");
+  text.value = text.value.toUpperCase();
+}
+
+function toLower() {
+  const text = document.getElementById("text");
+  text.value = text.value.toLowerCase();
+}
